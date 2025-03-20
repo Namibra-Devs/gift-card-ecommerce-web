6bd41d1808107685a4dba0d964a9d3a9c7248677
Tyler Gobka Bright <tgbright88@gmail.com>
2025-03-20 06:12:44 +0000

    Add assets and Colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div className="text-red-500 text-4xl">home</div>
-  )
-}
+    <div className="text-red-500 text-4xl text-center">
+      Gift Card And E-commerce
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,28 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        pink: "#F963D5",
-        glass: "#434343",
-        blur: "hsba(0, 0%, 100%, 0.1)"
+        white: "#FFFFFF",
+        grey: "#707070",
+        greynormal: "#0D0D0E",
+        greydark: "#0A0A0B",
+        greylight: "#F7F7F7",
+        greylightactive: "#B4B4B4",
+        overlay: "rgba(0, 0, 0, 0.1)",
+        greenlightactive: "#F7EFD3",
+        bluelightactive: "#CED7F8",
+        redlightactive: "#FFB0B1",
+        boltbg: "#EEEEEE",
+        walmartbg: "#1A75CF",
+        doordashbg: "#CD4C59",
+        amazonbg: "#232F3E",
+        playstationbg: "#01479C",
+        nintendobg: "#E60012",
+        ninetendobggrey: "#F1F1F1",
+        starbucksbg: "#016438",
+        spotifybg: "#1DB954",
+        netflixbg: "#E50914",
+        xboxbg: "#107C10",
+        hotelsbg: "#FBD7DB"
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
